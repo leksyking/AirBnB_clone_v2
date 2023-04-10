f860617ad1d093daa013cb184b4c6cc1eb27ed9f
Ogundipe Felix <85465631+leksyking@users.noreply.github.com>
2023-04-10 02:49:19 +0100

    update the ips

--- 3-deploy_web_static.py
+++ 3-deploy_web_static.py
@@ -7,7 +7,7 @@ from fabric.api import local
 from fabric.api import put
 from fabric.api import run
 
-env.hosts = ["104.196.168.90", "35.196.46.172"]
+env.hosts = ["54.89.118.196", "54.175.136.180"]
 
 
 def do_pack():
